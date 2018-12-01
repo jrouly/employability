2b57dc6557b73277e91bb0e367df0b69a497a7c4
Jean Michel Rouly <michel.rouly@gmail.com>
2018-12-01 12:53:38 -0500

    clean up ES import/export commands

--- elasticsearch/bin/lib.sh
+++ elasticsearch/bin/lib.sh
@@ -1,35 +1,47 @@
+# create_snapshot <repo> <snapshot>
+function create_snapshot() {
+  REPO=$1
+  SNAPSHOT=$2
+  if [[ -z $REPO ]] ; then echo "Missing Repo paramter." ; exit 1 ; fi
+  if [[ -z $SNAPSHOT ]] ; then echo "Missing Snapshot paramter." ; exit 1 ; fi
+  curl -XPUT "localhost:9200/_snapshot/$REPO/$SNAPSHOT?wait_for_completion=true"
+  echo ""
+}
+
+# restore_snapshot <repo> <snapshot>
+function restore_snapshot() {
+  REPO=$1
+  SNAPSHOT=$2
+  if [[ -z $REPO ]] ; then echo "Missing Repo paramter." ; exit 1 ; fi
+  if [[ -z $SNAPSHOT ]] ; then echo "Missing Snapshot paramter." ; exit 1 ; fi
+  curl -XPOST "localhost:9200/_snapshot/$REPO/$SNAPSHOT/_restore?wait_for_completion=true"
+  echo ""
+}
+
+# create_remote_repo
 function create_remote_repo() {
   curl -XPUT \
     -H 'Content-Type: application/json' \
-    localhost:9200/_snapshot/global \
+    localhost:9200/_snapshot/remote \
     -d '{
       "type": "url",
       "settings": {
         "url": "http://employability.rouly.net/snapshots/"
       }
     }'
+  echo ""
 }
 
+# create_local_repo
 function create_local_repo() {
   curl -XPUT \
     -H 'Content-Type: application/json' \
-    localhost:9200/_snapshot/global \
+    localhost:9200/_snapshot/local \
     -d '{
       "type": "fs",
       "settings": {
-        "location": "/usr/share/elasticsearch/snapshots/global"
+        "location": "/usr/share/elasticsearch/snapshots/local"
       }
     }'
-}
-
-function restore_snapshot() {
-  SNAPSHOT_ID=$1
-  if [[ -z $SNAPSHOT_ID ]] ; then echo "Missing Snapshot ID paramter." ; exit 1 ; fi
-  curl -XPOST "localhost:9200/_snapshot/global/$SNAPSHOT_ID/_restore"
-}
-
-function create_snapshot() {
-  SNAPSHOT_ID=$1
-  if [[ -z $SNAPSHOT_ID ]] ; then echo "Missing Snapshot ID paramter." ; exit 1 ; fi
-  curl -XPUT "localhost:9200/_snapshot/global/$SNAPSHOT_ID?wait_for_completion=true"
+  echo ""
 }
